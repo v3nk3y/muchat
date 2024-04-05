200fe654890f8632a03e8857ff32cdeebe7be6d5
v3nk3y <nagireddyvenkatesh@gmail.com>
2024-04-05 13:37:50 -0400

    muchat: Add skeletons for Navbar and Sidenav

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -1,11 +1,13 @@
+import Navbar from "@/components/Navbar";
+import Sidenav from "@/components/Sidenav";
 import { PropsWithChildren } from "react";
 
 const HomeLayout = ({ children }: PropsWithChildren) => {
   return (
     <main className="relative">
-      Navbar
+      <Navbar />
       <div className="flex">
-        SideBar
+        <Sidenav />
         <section className="flex flex-col flex-1 min-h-screen px-6 pb-6 pt-28 max-md:pb-14 sm:px-14">
           <div className="w-full">{children}</div>
         </section>
