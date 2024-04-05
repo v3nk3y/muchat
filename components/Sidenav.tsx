41c02544b63cd36b1cba0067d149d1a6c98ee4f6
v3nk3y <nagireddyvenkatesh@gmail.com>
2024-04-05 16:02:22 -0400

    muchat: Add and build sidenav links and basic view

--- components/Sidenav.tsx
+++ components/Sidenav.tsx
@@ -1,7 +1,41 @@
+"use client";
+import { sidenavLinks } from "@/constants";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 const Sidenav = () => {
-  return <div>Sidenav</div>;
+  // Get current pathname
+  const pathname = usePathname();
+  return (
+    <section
+      className="sticky left-0 top-0 h-screen w-fit flex flex-col justify-between p-6 pt-28 bg-gray-900 text-white
+     max-sm:hidden lg:w-[264px]"
+    >
+      <div className="flec flex-col flex-1 gap-6">
+        {sidenavLinks.map((link) => {
+          // StartsWith is for additional check in case we have other params in path
+          const isActive =
+            pathname === link.route || pathname.startsWith(link.route);
+          return (
+            <Link
+              href={link.route}
+              key={link.label}
+              className={cn(
+                "flex gap-4 items-center justify-start p-4 rounded-lg",
+                { "bg-blue-500": isActive }
+              )}
+            >
+              <p className="text-lg font-semibold max-lg:hidden">
+                {link.label}
+              </p>
+            </Link>
+          );
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Sidenav;
---
+++ constants/index.ts
@@ -0,0 +1,27 @@
+export const sidenavLinks = [
+  {
+    label: "Home",
+    route: "/",
+    imgUrl: "/icons/home.svg",
+  },
+  {
+    label: "Upcoming",
+    route: "/upcoming",
+    imgUrl: "/icons/upcoming.svg",
+  },
+  {
+    label: "Previous",
+    route: "/previous",
+    imgUrl: "/icons/previous.svg",
+  },
+  {
+    label: "Recordings",
+    route: "/recordings",
+    imgUrl: "/icons/video.svg",
+  },
+  {
+    label: "My Space",
+    route: "/my-space",
+    imgUrl: "/icons/my-space.svg",
+  },
+];
