7315aa1dc1a272d20bfaa8c3b7f755875634c6fc
v3nk3y <nagireddyvenkatesh@gmail.com>
2024-04-05 13:23:32 -0400

    muchat: Add skeleton route groups & layouts

---
+++ app/(root)/(home)/layout.tsx
@@ -0,0 +1,17 @@
+import { PropsWithChildren } from "react";
+
+const HomeLayout = ({ children }: PropsWithChildren) => {
+  return (
+    <main className="relative">
+      Navbar
+      <div className="flex">
+        SideBar
+        <section className="flex flex-col flex-1 min-h-screen px-6 pb-6 pt-28 max-md:pb-14 sm:px-14">
+          <div className="w-full">{children}</div>
+        </section>
+      </div>
+    </main>
+  );
+};
+
+export default HomeLayout;
---
+++ app/(root)/layout.tsx
@@ -0,0 +1,7 @@
+import { PropsWithChildren } from "react";
+
+const RootLayout = ({ children }: PropsWithChildren) => {
+  return <main>{children}</main>;
+};
+
+export default RootLayout;
