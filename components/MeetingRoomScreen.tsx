3d10a648408436e534d21f6cd654d9519f879017
v3nk3y <nagireddyvenkatesh@gmail.com>
2024-04-09 15:17:06 -0400

    muchat: Refactor: fix end call button routing issue

--- components/MeetingRoomScreen.tsx
+++ components/MeetingRoomScreen.tsx
@@ -10,7 +10,7 @@ import {
   useCallStateHooks,
 } from "@stream-io/video-react-sdk";
 import { LayoutList, Users } from "lucide-react";
-import { useSearchParams } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import { useState } from "react";
 import EndCallButton from "./EndCallButton";
 import {
@@ -25,6 +25,7 @@ import Loader from "./Loader";
 type CallLayoutType = "grid" | "speaker-left" | "speaker-right";
 
 const MeetingRoomScreen = () => {
+  const router = useRouter();
   const searchParams = useSearchParams();
   const isPersonalRoom = !!searchParams.get("personal");
   const [layout, setLayout] = useState<CallLayoutType>("speaker-left");
@@ -64,7 +65,7 @@ const MeetingRoomScreen = () => {
       </div>
       {/* video layout and call controls */}
       <div className="fixed bottom-0 flex w-full items-center justify-center gap-5 flex-wrap">
-        <CallControls />
+        <CallControls onLeave={() => router.push("/")} />
         <DropdownMenu>
           <div className="flex items-center">
             <DropdownMenuTrigger className="cursor-pointer rounded-2xl bg-slate-800 px-4 py-2 hover:bg-slate-700">
